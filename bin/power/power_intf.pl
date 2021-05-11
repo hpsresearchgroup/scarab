24f03bfeea4560b5d0c3350457b5c80aa54751c2
Ali Fakhrzadehgan <AliFakhr@users.noreply.github.com>
2021-05-11 18:57:42 -0500

    fixing the unit conversion bug in the power_model.pl perl file. (#97)

--- bin/power/power_intf.pl
+++ bin/power/power_intf.pl
@@ -274,7 +274,7 @@ sub parse_cacti_dram_output($$)
          $write_energy     (Write Energy nJ)     * $power_dram_write_stat     (POWER_DRAM_WRITE)\n");
     }
 
-    $values{"DYNAMIC"} *= 1e-9; # convert from nJoules to Joules
+    $values{"DYNAMIC"} *= 1.0e-9; # convert from nJoules to Joules
 
     my $time = $stats{"POWER_CYCLE"}[0]/$params{"POWER_INTF_REF_CHIP_FREQ"};
     if($debug) {
@@ -282,7 +282,7 @@ sub parse_cacti_dram_output($$)
       my $num_cycles = $stats{"POWER_CYCLE"}[0];
       my $core_freq  = $params{"POWER_INTF_REF_CHIP_FREQ"};
 
-      print("$time sec = $num_cycles (cycles) * $core_freq (freq Hz)\n");
+      print("$time sec = $num_cycles (cycles) / $core_freq (freq Hz)\n");
     }
 
     if ($debug) {
@@ -310,7 +310,7 @@ sub parse_cacti_dram_output($$)
       print("$total_leakage mW (STATIC) = $open_page_leakage (Leakage Power Open Page mW) + $io_leakage (Leakage Power I/O mW) + $refresh_leakage (Refresh power mW)");
     }
 
-    $values{"STATIC"} *= 1e-3; # convert to Watts
+    $values{"STATIC"} *= 1.0e-3; # convert to Watts
 
     return \%values;
 }
